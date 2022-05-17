c06ccb88f6e5b1f66c6ad4ac51e8e9eee5853454
Sadık Ahmet <sadikahmetozdemir@outlook.com>
2022-05-17 17:28:24 +0300

    Create-screens

---
+++ app/src/main/java/com/sadikahmetozdemir/invio/core/shared/remote/MovieResponseModel.kt
@@ -0,0 +1,8 @@
+package com.sadikahmetozdemir.invio.core.shared.remote
+
+data class MovieResponseModel(
+    val Genre: String,
+    val Plot: String,
+    val Poster: String,
+    val Title: String,
+)
